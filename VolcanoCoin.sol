9e3ef747c735b68cc718fbedf5588bf699bc1170
Panagiotis Karypiadis <pan.karyp@gmail.com>
2021-11-15 15:42:41 +0200

    create an event each time the supply changes

--- VolcanoCoin.sol
+++ VolcanoCoin.sol
@@ -2,28 +2,29 @@
 pragma solidity 0.8.0;
 
 contract VolcanoCoin {
-    
-    uint public supply;
-    
+    uint256 public supply;
+
     address owner;
-    
+
     modifier onlyOwner() {
-        if(msg.sender==owner) {
+        if (msg.sender == owner) {
             _;
         }
     }
-    
+
+    event SupplySet(uint256);
+
     constructor() {
-        supply=10000;
-        owner= msg.sender;
+        supply = 10000;
+        owner = msg.sender;
     }
-        
-    function getSupply() public view returns(uint) {
+
+    function getSupply() public view returns (uint256) {
         return supply;
     }
-    
-    function setSupply(uint _newSupply) public onlyOwner{
+
+    function setSupply(uint256 _newSupply) public onlyOwner {
         supply = _newSupply;
+        emit SupplySet(_newSupply);
     }
-    
 }
